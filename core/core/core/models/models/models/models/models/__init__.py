ba3b2ef634eb8c3ea62430920fa45aa8cfe5fa60
bogdanovbs07 <bogdanovbs07@yandex.ru>
2026-03-24 22:09:03 +0300

    Create __init__.py

---
+++ core/core/core/models/models/models/models/models/__init__.py
@@ -0,0 +1,6 @@
+from models.user import User
+from models.role import Role
+from models.permission import Permission
+from models.token import RefreshToken
+
+__all__ = ["User", "Role", "Permission", "RefreshToken"]
